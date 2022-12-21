6c06452bc2c5ff30572c59c84043b390e75fbd64
Sandip Paul <paulsandip2110@gmail.com>
2022-12-22 00:58:38 +0530

    Add files via upload

---
+++ precache-manifest.a67996c34e842246224ac086d121295e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab8874ec6c1ed113a0081d88f8bf21df",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8647cfe8414238cb6221",
+    "url": "/static/css/main.60eb8094.chunk.css"
+  },
+  {
+    "revision": "9a3fe72ca40a23a91c8f",
+    "url": "/static/js/2.61263b80.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/static/js/2.61263b80.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8647cfe8414238cb6221",
+    "url": "/static/js/main.d21d1078.chunk.js"
+  },
+  {
+    "revision": "a5ad72c7b035ab9759f9",
+    "url": "/static/js/runtime-main.9dfdc2fd.js"
+  },
+  {
+    "revision": "8698e162cb63034727c28e456466452c",
+    "url": "/static/media/cropData.8698e162.csv"
+  }
+]);
